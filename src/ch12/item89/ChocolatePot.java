3ed6ef86bb316f22151fc65a1918aa4562ee7468
MARI <mari20201101@gmail.com>
2021-12-11 15:21:43 +0900

    Learn singlton serialization

--- src/ch12/item89/ChocolatePot.java
+++ src/ch12/item89/ChocolatePot.java
@@ -4,11 +4,17 @@ import java.io.Serializable;
 
 public class ChocolatePot implements Serializable {
 
+    private String chocolate = "chocochocho";
     public static final ChocolatePot INSTANCE = new ChocolatePot();
     private ChocolatePot(){
-        System.out.println("i am singlton choco pot v6");
+        System.out.println("i am singlton choco pot ");
     }
 
+    public void print(){
+        System.out.println("*****************");
+        System.out.println(chocolate);
+        System.out.println("*****************");
+    }
     private Object readResolve(){
         return INSTANCE;
     }
